8703c5cb429e8bf9f0efda13a7248261b651f0b5
Vantaablack <razakmohamed2206@gmail.com>
2026-03-18 13:34:44 +0100

    Fix: boucle infinie pour garder le conteneur actif

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,5 +3,15 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Application Boutique démarrée");
+
+        // Boucle infinie pour garder le conteneur actif
+        while (true) {
+            try {
+                Thread.sleep(10000); // 10 secondes
+                System.out.println("Application toujours en cours...");
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
