49d6cad33f281a03e5ffe03bbddf92b1808d6513
Vantaablack <razakmohamed2206@gmail.com>
2026-03-18 13:51:22 +0100

    Main modifié

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,17 +1,33 @@
 package org.example;
 
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Application Boutique démarrée");
+import com.sun.net.httpserver.HttpServer;
+import com.sun.net.httpserver.HttpExchange;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
 
-        // Boucle infinie pour garder le conteneur actif
-        while (true) {
-            try {
-                Thread.sleep(10000); // 10 secondes
-                System.out.println("Application toujours en cours...");
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
+public class Main {
+    public static void main(String[] args) throws IOException {
+        int port = 8080;
+
+        String envPort = System.getenv("APP_PORT");
+        if (envPort != null && !envPort.isBlank()) {
+            port = Integer.parseInt(envPort);
         }
+
+        HttpServer server = HttpServer.create(new InetSocketAddress(port), 0);
+
+        server.createContext("/", (HttpExchange exchange) -> {
+            String response = "Application Boutique demarree";
+            exchange.sendResponseHeaders(200, response.getBytes().length);
+            try (OutputStream os = exchange.getResponseBody()) {
+                os.write(response.getBytes());
+            }
+        });
+
+        server.setExecutor(null);
+        server.start();
+
+        System.out.println("Serveur HTTP demarre sur le port " + port);
     }
 }
